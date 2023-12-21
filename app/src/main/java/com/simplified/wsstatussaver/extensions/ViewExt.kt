dd2bbe81b4b760d0bb76ae109f7bf7112dd83b00
Christians Martínez Alvarado <mardous.contact@gmail.com>
2023-12-21 09:09:32 -0400

    Add code to hide and show the bottom navigation view dynamically

--- app/src/main/java/com/simplified/wsstatussaver/extensions/ViewExt.kt
+++ app/src/main/java/com/simplified/wsstatussaver/extensions/ViewExt.kt
@@ -13,6 +13,15 @@
  */
 package com.simplified.wsstatussaver.extensions
 
+import android.animation.ValueAnimator
+import android.graphics.drawable.BitmapDrawable
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.core.animation.doOnEnd
+import androidx.core.view.drawToBitmap
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -20,6 +29,89 @@ import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigationrail.NavigationRailView
+
+private const val WHATSAVE_ANIM_TIME = 350L
+
+/**
+ * Potentially animate showing a [NavigationBarView].
+ *
+ * Abruptly changing the visibility leads to a re-layout of main content, animating
+ * `translationY` leaves a gap where the view was that content does not fill.
+ *
+ * Instead, take a snapshot of the view, and animate this in, only changing the visibility (and
+ * thus layout) when the animation completes.
+ */
+fun NavigationBarView.show() {
+    if (this is NavigationRailView) return
+    if (isVisible) return
+
+    val parent = parent as ViewGroup
+    // View needs to be laid out to create a snapshot & know position to animate. If view isn't
+    // laid out yet, need to do this manually.
+    if (!isLaidOut) {
+        measure(
+            View.MeasureSpec.makeMeasureSpec(parent.width, View.MeasureSpec.EXACTLY),
+            View.MeasureSpec.makeMeasureSpec(parent.height, View.MeasureSpec.AT_MOST)
+        )
+        layout(parent.left, parent.height - measuredHeight, parent.right, parent.height)
+    }
+
+    val drawable = BitmapDrawable(context.resources, drawToBitmap())
+    drawable.setBounds(left, parent.height, right, parent.height + height)
+    parent.overlay.add(drawable)
+    ValueAnimator.ofInt(parent.height, top).apply {
+        duration = WHATSAVE_ANIM_TIME
+        interpolator = AnimationUtils.loadInterpolator(context, android.R.interpolator.accelerate_decelerate)
+        addUpdateListener {
+            val newTop = it.animatedValue as Int
+            drawable.setBounds(left, newTop, right, newTop + height)
+        }
+        doOnEnd {
+            parent.overlay.remove(drawable)
+            isVisible = true
+        }
+        start()
+    }
+}
+
+/**
+ * Potentially animate hiding a [BottomNavigationView].
+ *
+ * Abruptly changing the visibility leads to a re-layout of main content, animating
+ * `translationY` leaves a gap where the view was that content does not fill.
+ *
+ * Instead, take a snapshot, instantly hide the view (so content lays out to fill), then animate
+ * out the snapshot.
+ */
+fun NavigationBarView.hide() {
+    if (this is NavigationRailView) return
+    if (isGone) return
+
+    if (!isLaidOut) {
+        isGone = true
+        return
+    }
+
+    val drawable = BitmapDrawable(context.resources, drawToBitmap())
+    val parent = parent as ViewGroup
+    drawable.setBounds(left, top, right, bottom)
+    parent.overlay.add(drawable)
+    isGone = true
+    ValueAnimator.ofInt(top, parent.height).apply {
+        duration = WHATSAVE_ANIM_TIME
+        interpolator = AnimationUtils.loadInterpolator(context, android.R.interpolator.accelerate_decelerate)
+        addUpdateListener {
+            val newTop = it.animatedValue as Int
+            drawable.setBounds(left, newTop, right, newTop + height)
+        }
+        doOnEnd {
+            parent.overlay.remove(drawable)
+        }
+        start()
+    }
+}
 
 fun RecyclerView.Adapter<*>?.isNullOrEmpty(): Boolean = this == null || this.itemCount == 0
 
