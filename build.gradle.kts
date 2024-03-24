965919334a2e4bdda4547c9877de318cced225d5
Christians Martínez Alvarado <mardous.contact@gmail.com>
2024-03-24 15:42:06 -0400

    Upgrade AGP and Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
-    id("com.google.devtools.ksp") version "1.9.21-1.0.16" apply false
+    id("com.android.application") version "8.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
     id("com.google.firebase.crashlytics") version "2.9.9" apply false
     id("androidx.navigation.safeargs") version "2.7.6" apply false
