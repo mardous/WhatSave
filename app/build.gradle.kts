09f6c24dc1787064a11c18e90f8c4cbfc9f973c0
mardous <mardous.contact@gmail.com>
2024-05-07 17:47:39 -0400

    Bump dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,11 +80,11 @@ android {
 dependencies {
     // Google/JetPack
     //https://developer.android.com/jetpack/androidx/versions
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.annotation:annotation:1.7.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.fragment:fragment-ktx:1.7.0")
 
     val lifecycleVersion = "2.7.0"
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
@@ -107,25 +107,25 @@ dependencies {
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
 
     //Firebase
-    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-crashlytics-ktx")
 
-    val koinVersion = "3.5.3"
+    val koinVersion = "3.5.6"
     implementation("io.insert-koin:koin-core:$koinVersion")
     implementation("io.insert-koin:koin-android:$koinVersion")
 
-    val retrofitVersion = "2.10.0"
+    val retrofitVersion = "2.11.0"
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("io.github.g00fy2:versioncompare:1.5.0")
-    implementation("io.michaelrocks:libphonenumber-android:8.13.28")
+    implementation("io.michaelrocks:libphonenumber-android:8.13.35")
     implementation("org.ocpsoft.prettytime:prettytime:5.0.4.Final")
 
     // Kotlin
