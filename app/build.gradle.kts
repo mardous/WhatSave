ea242a476e4e6c5c9af3159d2b7a4c4431c54263
mardous <mardous.contact@gmail.com>
2024-08-27 11:41:52 -0400

    Use version catalog for dependency declaration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,12 +3,12 @@ import java.util.Properties
 val isNormalBuild: Boolean by rootProject.extra
 
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.agp)
+    alias(libs.plugins.kotlin.android)
     id("kotlin-parcelize")
     id("kotlin-kapt")
-    id("com.google.devtools.ksp")
-    id("androidx.navigation.safeargs")
+    alias(libs.plugins.google.ksp)
+    alias(libs.plugins.androidx.safeargs)
 }
 
 if (isNormalBuild) {
@@ -105,57 +105,51 @@ fun Properties.property(key: String) =
 dependencies {
     // Google/JetPack
     //https://developer.android.com/jetpack/androidx/versions
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.annotation:annotation:1.8.1")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.2")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.splashscreen)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.fragment.ktx)
 
-    val lifecycleVersion = "2.8.4"
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
-    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion")
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.lifecycle.viewmodel.ktx)
+    implementation(libs.lifecycle.livedata.ktx)
+    implementation(libs.lifecycle.common.java8)
 
-    val navigationVersion = "2.7.7"
-    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
-    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+    implementation(libs.navigation.fragment.ktx)
+    implementation(libs.navigation.ui.ktx)
 
-    val roomVersion = "2.6.1"
-    implementation("androidx.room:room-ktx:$roomVersion")
-    ksp("androidx.room:room-compiler:$roomVersion")
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
 
-    implementation("androidx.loader:loader:1.1.0")
-    implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.viewpager2:viewpager2:1.1.0")
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("com.google.android.material:material:1.12.0")
+    implementation(libs.androidx.loader)
+    implementation(libs.androidx.cardview)
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.viewpager2)
+    implementation(libs.androidx.swiperefreshlayout)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.preference.ktx)
+    implementation(libs.material.components)
 
     //Firebase
-    "normalImplementation"(platform("com.google.firebase:firebase-bom:33.1.2"))
-    "normalImplementation"("com.google.firebase:firebase-analytics-ktx")
-    "normalImplementation"("com.google.firebase:firebase-crashlytics-ktx")
+    "normalImplementation"(platform(libs.firebase.bom))
+    "normalImplementation"(libs.firebase.analytics)
+    "normalImplementation"(libs.firebase.crashlytics)
 
-    val koinVersion = "3.5.6"
-    implementation("io.insert-koin:koin-core:$koinVersion")
-    implementation("io.insert-koin:koin-android:$koinVersion")
+    implementation(libs.koin.core)
+    implementation(libs.koin.android)
 
-    val retrofitVersion = "2.11.0"
-    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("com.google.code.gson:gson:2.11.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("io.github.g00fy2:versioncompare:1.5.0")
-    implementation("io.michaelrocks:libphonenumber-android:8.13.35")
-    implementation("org.ocpsoft.prettytime:prettytime:5.0.4.Final")
-    implementation("com.h6ah4i.android.widget.advrecyclerview:advrecyclerview:1.0.0")
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.okhttp3.logging)
+    implementation(libs.gson)
+    implementation(libs.glide)
+    implementation(libs.versioncompare)
+    implementation(libs.libphonenumber)
+    implementation(libs.prettytime)
+    implementation(libs.advrecyclerview)
 
     // Kotlin
-    val kotlinCoroutinesVersion = "1.8.1"
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$kotlinCoroutinesVersion")
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
-    id("androidx.navigation.safeargs") version "2.7.6" apply false
+    alias(libs.plugins.agp) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.google.ksp) apply false
+    alias(libs.plugins.androidx.safeargs) apply false
 }
 
 buildscript {
@@ -14,8 +14,8 @@ buildscript {
     }
     dependencies {
         if (isNormalBuild) {
-            classpath("com.google.gms:google-services:4.4.0")
-            classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
+            classpath(libs.gms.plugin)
+            classpath(libs.firebase.crashlytics.plugin)
         }
     }
 }
