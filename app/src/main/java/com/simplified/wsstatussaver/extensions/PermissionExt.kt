4b1db7e34d5923780b5b6e0f8dc7f25bc685fb39
mardous <mardous.contact@gmail.com>
2025-05-09 18:57:29 -0400

    refactor: simplify directory access request
    This change simplifies the directory access request flow by directly using `ActivityResultContracts.OpenDocumentTree()` instead of a custom intent with `ACTION_OPEN_DOCUMENT_TREE`. This should avoid recent crashes related to ActivityNotFoundException.

--- app/src/main/java/com/simplified/wsstatussaver/extensions/PermissionExt.kt
+++ app/src/main/java/com/simplified/wsstatussaver/extensions/PermissionExt.kt
@@ -18,19 +18,12 @@ import android.Manifest.permission.READ_MEDIA_IMAGES
 import android.Manifest.permission.READ_MEDIA_VIDEO
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.UriPermission
 import android.net.Uri
 import android.os.Build
-import android.os.storage.StorageManager
-import android.provider.DocumentsContract
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.core.content.IntentCompat
-import androidx.core.content.getSystemService
-import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
@@ -103,22 +96,20 @@ fun Fragment.requestWithoutOnboard() = requireActivity().requestWithoutOnboard()
 
 fun Fragment.requestPermissions() = requireActivity().requestPermissions(true)
 
-fun Fragment.takePermissions(result: ActivityResult, isShowToast: Boolean = true): Boolean {
+fun Fragment.takePermissions(selectedUri: Uri?, isShowToast: Boolean = true): Boolean {
     val context = this.context
-    if (result.resultCode == Activity.RESULT_OK && context != null) {
-        val uri = result.data?.data ?: return false
-        val directory = uri.toWhatsAppDirectory()
-        if (directory != null && !directory.isLegacy) {
-            if (!directory.isReadable(context)) { // Check if access has not already been given previously
-                val mask = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
-                context.contentResolver.takePersistableUriPermission(uri, mask)
-                if (isShowToast) showToast(R.string.permissions_granted_successfully)
-                syncPermissions()
-                return true
-            }
-        } else {
-            if (isShowToast) showToast(R.string.select_the_correct_location, Toast.LENGTH_LONG)
+    if (selectedUri == null || context == null) return false
+    val directory = selectedUri.toWhatsAppDirectory()
+    if (directory != null && !directory.isLegacy) {
+        if (!directory.isReadable(context)) { // Check if access has not already been given previously
+            val mask = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+            context.contentResolver.takePersistableUriPermission(selectedUri, mask)
+            if (isShowToast) showToast(R.string.permissions_granted_successfully)
+            syncPermissions()
+            return true
         }
+    } else {
+        if (isShowToast) showToast(R.string.select_the_correct_location, Toast.LENGTH_LONG)
     }
     return false
 }
@@ -138,21 +129,4 @@ fun Fragment.syncPermissions() {
             dir.releasePermissions(requireContext())
         }
     }
-}
-
-fun Fragment.directoryAccessRequestIntent(directory: WaDirectory = WaDirectory.Media): Intent {
-    val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-    if (hasQ()) {
-        val storageManager = requireContext().getSystemService<StorageManager>()!!
-        val treeIntent = storageManager.primaryStorageVolume.createOpenDocumentTreeIntent()
-        val uri = IntentCompat.getParcelableExtra(treeIntent, DocumentsContract.EXTRA_INITIAL_URI, Uri::class.java)
-        val scheme = uri.toString().replace("/root/", "/document/") + directory.path.encodedUrl()
-        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, scheme.toUri())
-    }
-    intent.setFlags(
-        Intent.FLAG_GRANT_READ_URI_PERMISSION or
-        Intent.FLAG_GRANT_WRITE_URI_PERMISSION or
-        Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION
-    )
-    return intent
 }
