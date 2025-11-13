46ef649d6143c5ac976b8319fedff916df875278
Christian <37917778+mardous@users.noreply.github.com>
2025-11-13 18:28:41 -0400

    chore: remove duplicate license header

--- app/src/main/java/com/simplified/wsstatussaver/model/WaFile.kt
+++ app/src/main/java/com/simplified/wsstatussaver/model/WaFile.kt
@@ -11,19 +11,6 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
  */
-/*
- * Copyright (C) 2025 Christians Martínez Alvarado
- *
- * Licensed under the GNU General Public License v3
- *
- * This is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by
- * the Free Software Foundation either version 3 of the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- */
 package com.simplified.wsstatussaver.model
 
 import android.net.Uri
